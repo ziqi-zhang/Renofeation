ad2c51aadf8c2a5f3aa5bd4e52bdec1bc8868ba6
ziqi <ziqi_zhang@pku.edu.cn>
2020-08-19 12:06:25 +0800

    backdoor script

--- backdoor/examples/divmag_do.sh
+++ backdoor/examples/divmag_do.sh
@@ -15,10 +15,11 @@ WEIGHT_DECAY=1e-4
 PORTION=(0.2 0.5 0.7 0.9)
 RATIO=(0.0 0.5 0.7 0.9)
 
-for i in 3 5
+for j in 0 
 do
-    for j in 0
+    for i in 0 1 2
     do
+
     DATASET=${DATASETS[i]}
     DATASET_NAME=${DATASET_NAMES[i]}
     DATASET_ABBR=${DATASET_ABBRS[i]}
@@ -28,7 +29,9 @@ do
     ratio=${RATIO[j]}
     NAME=fixed_${DATASET_ABBR}_${ratio}
     #NAME=random_${DATASET_ABBR}_${ratio}
-    newDIR=results/backdoor/baseline/
+    newDIR=results/backdoor/divmag_do
+    # newDIR=results/test
+    teacher_dir=results/backdoor/baseline/fixed_${DATASET_ABBR}_${ratio}
 
     CUDA_VISIBLE_DEVICES=$1 \
     python -u py_qianyi.py \
@@ -52,7 +55,19 @@ do
     --argportion ${portion} \
     --backdoor_update_ratio ${ratio} \
     --teacher_method backdoor_finetune \
+    --checkpoint $teacher_dir/teacher_ckpt.pth \
+    --student_method global_datasetgrad_divmag_iter \
     --fixed_pic \
+    --train_all \
+    --prune_interval 100 \
+    --weight_total_ratio 0.05 \
+    --weight_ratio_per_prune 0 \
+    --weight_init_prune_ratio 0.05 \
+    --trial_iteration 3000 \
+    --trial_lr ${lr} \
+    --trial_momentum 0.9 \
+    --trial_weight_decay 0 \
+    --dropout 1e-1 \
     &
 
     done
--- backdoor/examples/reno_nobackdoor.sh
+++ backdoor/examples/reno_nobackdoor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-iter=10000
+iter=90000
 id=1
 splmda=0
-lmda=0
 layer=1234
-mmt=0
+lr=1e-2
+wd=5e-3
+mmt=0.9
+lmda=5e0
 
 DATASETS=(MIT_67 stanford_dog GTSRB CUB_200_2011 Flower_102 stanford_40 stanford_dog GTSRB)
 DATASET_NAMES=(MIT67Data SDog120Data GTSRBData CUB200Data Flower102Data Stanford40Data SDog120Data GTSRBData)
@@ -15,10 +17,11 @@ WEIGHT_DECAY=1e-4
 PORTION=(0.2 0.5 0.7 0.9)
 RATIO=(0.0 0.5 0.7 0.9)
 
-for i in 3 5
+for j in 0 
 do
-    for j in 0
+    for i in 3 4
     do
+
     DATASET=${DATASETS[i]}
     DATASET_NAME=${DATASET_NAMES[i]}
     DATASET_ABBR=${DATASET_ABBRS[i]}
@@ -28,7 +31,9 @@ do
     ratio=${RATIO[j]}
     NAME=fixed_${DATASET_ABBR}_${ratio}
     #NAME=random_${DATASET_ABBR}_${ratio}
-    newDIR=results/backdoor/baseline/
+    newDIR=results/backdoor/reno_nobackdoor_nodo
+    # newDIR=results/test
+    teacher_dir=results/backdoor/baseline/fixed_${DATASET_ABBR}_${ratio}
 
     CUDA_VISIBLE_DEVICES=$1 \
     python -u py_qianyi.py \
@@ -52,8 +57,21 @@ do
     --argportion ${portion} \
     --backdoor_update_ratio ${ratio} \
     --teacher_method backdoor_finetune \
+    --student_method finetune \
     --fixed_pic \
+    --train_all \
+    --prune_interval 100 \
+    --weight_total_ratio 0.05 \
+    --weight_ratio_per_prune 0 \
+    --weight_init_prune_ratio 0.05 \
+    --trial_iteration 3000 \
+    --trial_lr ${lr} \
+    --trial_momentum 0.9 \
+    --trial_weight_decay 0 \
+    --reinit \
     &
+    # --student_ckpt ${newDIR}/${NAME}/ckpt.pth \
+    # &
 
     done
 done
